97ada781d9724a3ad5b9a46fd4630ab28845461e
Morgan Le Gal <morgan@linxo.com>
2017-10-23 20:49:34 +0200

    Initial Commit

---
+++ LinxoNUI/LinxoNUI.h
@@ -0,0 +1,19 @@
+//
+//  LinxoNUI.h
+//  LinxoNUI
+//
+//  Created by Morgan  on 23/10/2017.
+//  Copyright © 2017 Linxo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinxoNUI.
+FOUNDATION_EXPORT double LinxoNUIVersionNumber;
+
+//! Project version string for LinxoNUI.
+FOUNDATION_EXPORT const unsigned char LinxoNUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinxoNUI/PublicHeader.h>
+
+

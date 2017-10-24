23154f583985230c9f7fa401bd95f536002ee6ac
Morgan Le Gal <morgan@linxo.com>
2017-10-24 10:23:43 +0200

    added header

--- LinxoNUI/LinxoNUI.h
+++ LinxoNUI/LinxoNUI.h
@@ -16,3 +16,4 @@ FOUNDATION_EXPORT const unsigned char LinxoNUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LinxoNUI/PublicHeader.h>
 
+#import "LinxoNUIInit.h"
